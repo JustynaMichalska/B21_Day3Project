701e386d4b85649a1d17ee288749afeef16d0b96
JustynaMichalska <74165286+JustynaMichalska@users.noreply.github.com>
2020-11-12 10:10:48 -0600

    Learning new stuff

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
 	// write your code here
         //batch 21 is great
         //B21 is dedicated
+        //B21 is successful tester
+        // its great to learn new stuff
     }
 }
